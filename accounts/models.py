9f836dcaf76b4e3a3f1831b917d5e7c70e2c5e2c
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2022-04-16 22:26:35 +0200

    fix import in accouts/models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,3 @@
-from operator import mod
-from statistics import mode
 from django.db import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import AbstractUser
