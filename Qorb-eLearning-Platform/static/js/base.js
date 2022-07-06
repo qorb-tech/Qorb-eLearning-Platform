b6af2e6ecd0cd970857303acfeefa251eb93003d
Waleed Negm <55960681+waleednegm@users.noreply.github.com>
2022-07-06 21:53:16 +0200

    Update base.js

--- Qorb-eLearning-Platform/static/js/base.js
+++ Qorb-eLearning-Platform/static/js/base.js
@@ -4,7 +4,7 @@ var image1 = document.getElementById("image1");
 var image2 = document.getElementById("image2");
 var image3 = document.getElementById("image3");
 var image5 = document.getElementById("image5");
-
+var image6 = document.getElementById("image6");
 
 //---hover account
 function hoverr1(){
@@ -39,6 +39,16 @@ function hoverr2(){
   function without_hover5(){
    image5.src = '/static/images/icons/dashboard/exams.svg' ;
   }
+  
+   //---hover orders
+  function hoverr6(){
+    image6.src ='/static/images/icons/hover/Icon_attention_solid.svg';
+    
+   }
+   function without_hover6(){
+    image6.src = '/static/images/icons/nohover/Icon_attention_solid.svg';
+ 
+   }
   function teacher_dash(){
     for (var i = 0; i < profile_feature_active.length; i++) {
         var current = profile_features.getElementsByClassName("activee");
@@ -93,4 +103,19 @@ function quiz(){
           image5.src ='/static/images/icons/dashboard/oppsite/Group 184.svg' ;
           console.log("quize")
     }
+}
+
+function orders(){
+  for (var i = 0; i < profile_feature_active.length; i++) {
+      var current = profile_features.getElementsByClassName("activee");
+      // If there's no active class
+      if (current.length > 0) {
+        current[0].className = current[0].className.replace(" activee", "");
+        image6.src = '/static/images/icons/nohover/Icon_attention_solid.svg';
+       
+      }
+        profile_feature_active[4].classList.add("activee")
+        image6.src ='/static/images/icons/hover/Icon_attention_solid.svg';
+        console.log("orders")
+  }
 }
